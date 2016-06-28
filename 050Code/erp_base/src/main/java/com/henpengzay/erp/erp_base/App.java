68638c5585d85430ed4f3e37d83dd7fa34f51ea3
smith-dog <137826142@qq.com>
2016-06-28 14:12:56 +0800

    工程配置

---
+++ 050Code/erp_base/src/main/java/com/henpengzay/erp/erp_base/App.java
@@ -0,0 +1,13 @@
+package com.henpengzay.erp.erp_base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
